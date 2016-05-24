24eb8ec7e971053d79ef59011705549e7cc95509
Navneet Kaur <navneet.kaur@compunnel.in>
2016-05-24 18:09:24 +0530

    commiting user controller and services

--- src/riskcanvas/dao/user/UserDao.java
+++
@@ -1,9 +0,0 @@
-package riskcanvas.dao.user;
-import riskcanvas.dao.DatabaseConnection;
-import riskcanvas.model.AddUser;
-
-public interface UserDao {
-
-	public  boolean AddUser(AddUser adduser,DatabaseConnection connection);
-	
-}
--- src/riskcanvas/service/UserServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package riskcanvas.service;
-import riskcanvas.dao.user.*;
-public class UserServiceImpl {
-	
-UserDao userdao;
-
-}
